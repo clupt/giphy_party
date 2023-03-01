890bb7f49bece96c88abe651597f5b08f4571a9b
Claudia Lam <claudiaslam@gmail.com>
2023-02-28 17:57:18 -0800

    add getGiphy function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Let's get this party started!");
+"use strict";
+
+async function getGiphy(evt) {
+  evt.preventDefault();
+
+  let search = $("#search").val();
+
+  let response = await axios.get("api.giphy.com/v1/gifs/search", {
+    params: { search },
+  });
+
+  console.log("response", response);
+
+  $(".giphy-container").html(response.data);
+}
+
+$(".form").submit(getGiphy);
