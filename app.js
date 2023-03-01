d51f208a129a455119552116fef2fb1b30e18a6f
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-03-01 10:30:10 -0600

    fix pseudocode

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const api_key = 'oTB7fw1Fst1HG5FfXlUR26gzAmmiJW0D';
 const gif_url = 'api.giphy.com/v1/gifs/search';
 const giphyContainer = document.querySelector('.giphy-container');
 
+/**
+ * function called getGiphy
+ *
+ * listens to the click event on the form
+ * captures the value being search for
+ * awaits the promise from the axios get request and stores the resulting data
+ * specifically gets the url of the first gif from that data
+ *
+ * then calls the addGiphy function with the url from that response data
+ */
 async function getGiphy(evt) {
   console.log("getgiphy has fired");
   evt.preventDefault();
@@ -20,12 +30,17 @@ async function getGiphy(evt) {
   addGiphy(urlOfFirstGif);
 }
 
+/**
+ * function called addGiphy
+ *
+ * takes in the url from the response data from getGiphy
+ * makes an <img> from that source and appends it the the container
+ */
 async function addGiphy(url) {
   console.log("addGiphy has fired");
   const newGifImg = `<img src='${url}'/>`;
 
   console.log("newGifImg=", newGifImg);
-  // $('giphy-container').append(newGifImg);
   $('.giphy-container').append(newGifImg);
 }
 
